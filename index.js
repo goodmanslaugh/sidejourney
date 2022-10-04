d3ca2e5190b842e414c082c22c07dbe4c4b0ffb1
Sohan A <sohan@btr-india.co.in>
2022-10-04 23:09:34 +0530

    update variable name

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const API_URL = "https://api.github.com/users/";
+const BASEURL = "https://api.github.com/users/";
 async function getUser(username) {
-  const resp = await fetch(API_URL + username);
+  const resp = await fetch(BASEURL + username);
   const respData = await resp.json();
 
   getRepos(username);
 }
 async function getRepos(username) {
-  const resp = await fetch(API_URL + username + "/repos");
+  const resp = await fetch(BASEURL + username + "/repos");
   const respData = await resp.json();
   console.log(respData);
 }
